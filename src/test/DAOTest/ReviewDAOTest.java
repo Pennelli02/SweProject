8852a07b9d01aab00d90c63e5bd003a1ec06953e
lorenzo pennelli <161378352+Pennelli02@users.noreply.github.com>
2025-05-06 16:55:07 +0200

    end reviewDAOTest

--- src/test/DAOTest/ReviewDAOTest.java
+++ src/test/DAOTest/ReviewDAOTest.java
@@ -1,34 +1,117 @@
 package test.DAOTest;
 
+import BusinessLogic.UserController;
+import DAO.AccommodationDAO;
+import DAO.ReviewDAO;
+import DAO.UserDAO;
+import DomainModel.*;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ReviewDAOTest {
+    private ReviewDAO reviewDAO;
+    private UserController userController;
+    private UserDAO userDAO;
+    private RegisterUser registerUser;
+    private AccommodationDAO accommodationDAO;
+
+    LocalDateTime now = LocalDateTime.now();
+    int accommodationId;
+
+    private final String testEmail = "test.user@example.com";
+    private final String testPassword = "Test123!";
+    private final String testUsername = "testuser";
+    private final String testName = "Test";
+    private final String testSurname = "User";
+    private final Location testLocation= Location.Nothing;
 
     @BeforeEach
-    void setUp() {
+    void setUp() throws SQLException, ClassNotFoundException {
+        userController = new UserController();
+        userDAO = new UserDAO();
+        reviewDAO = new ReviewDAO();
+        accommodationDAO = new AccommodationDAO();
+
+        registerUser=userController.register(testEmail,testPassword,testUsername,testName,testSurname,testLocation);
+
+        accommodationDAO.addAccommodation(
+                "Family Spa Apartment",
+                "Via delle Terme 12",
+                "Test",
+                2,
+                AccommodationType.Apartment,
+                150.0f,
+                now.plusDays(5),
+                now.plusDays(20),
+                "Appartamento familiare con centro benessere incluso.",
+                AccommodationRating.FourStar,
+                false,
+                true,
+                false,
+                false,
+                true,
+                true,
+                true,
+                true,
+                true,
+                3,
+                true,
+                4
+        );
+        SearchParameters sp= SearchParametersBuilder.newBuilder("Test").build();
+        Accommodation acc= accommodationDAO.getAccommodationByParameter(sp).getFirst();
+        accommodationId=acc.getId();
     }
 
     @AfterEach
     void tearDown() {
+        if(registerUser!=null){
+            userDAO.removeUser(registerUser.getId());
+        }
+        accommodationDAO.deleteAccommodation(accommodationId);
     }
 
     @Test
     void getReviewByUser() {
+        ArrayList<Review> reviews=assertDoesNotThrow(()->reviewDAO.getReviewByUser(registerUser));
+        assertNotNull(reviews);
+        assertTrue(reviews.isEmpty());
+
+        // aggiungiamo delle recensioni per test
+        reviewDAO.addReview(registerUser, accommodationDAO.getAccommodationByID(accommodationId), "test test test", AccommodationRating.FourStar);
+        reviews=assertDoesNotThrow(()->reviewDAO.getReviewByUser(registerUser));
+        assertNotNull(reviews);
+        assertFalse(reviews.isEmpty());
     }
 
     @Test
     void removeReview() {
+        //aggiungo una recensione per il test
+        reviewDAO.addReview(registerUser, accommodationDAO.getAccommodationByID(accommodationId), "test test test", AccommodationRating.FourStar);
+        Review review= reviewDAO.getReviewByUser(registerUser).getFirst();
+        assertDoesNotThrow(()->reviewDAO.removeReview(review.getReviewID()));
+
+        assertTrue(reviewDAO.getReviewByUser(registerUser).isEmpty());
     }
 
     @Test
     void addReview() {
+        assertDoesNotThrow(()->reviewDAO.addReview(registerUser, accommodationDAO.getAccommodationByID(accommodationId), "test test test", AccommodationRating.FourStar));
+        assertNotNull(reviewDAO.getReviewByUser(registerUser).getFirst());
     }
 
     @Test
     void getReviewByAccommodation() {
+        //aggiungiamo una recensione per il test
+        reviewDAO.addReview(registerUser, accommodationDAO.getAccommodationByID(accommodationId), "test test test", AccommodationRating.FourStar);
+        Review review= reviewDAO.getReviewByAccommodation(accommodationDAO.getAccommodationByID(accommodationId)).getFirst();
+        assertNotNull(review);
     }
 }
